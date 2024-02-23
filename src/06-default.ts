aba4568e319e2a084f2563b4a953e2221a9d6f11
SirAdan <lpelcastre@bomuca.com>
2024-02-23 08:46:41 -0800

    [ADD] default params

---
+++ src/06-default.ts
@@ -0,0 +1,23 @@
+export const createProduct = (
+  id: string | number,
+  isNew: boolean = true,
+  stock: number = 10
+) => {
+  return {
+    id,
+    isNew,
+    stock,
+  };
+};
+
+const p1 = createProduct(1, true, 12);
+console.log(p1);
+
+const p2 = createProduct(1);
+console.log(p2);
+
+const p3 = createProduct(1, false, 0);
+console.log(p3);
+
+const p4 = createProduct('2efgh', true);
+console.log(p4);
